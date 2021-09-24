3c704c9f1318c9acd7b0682078ce241a6bde10e5
z1004man@naver.com <z1004man@naver.com>
2021-09-24 22:49:13 +0900

    [feature] API 코드 작성 - User component 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,69 @@
-import {useEffect, useState} from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, {useEffect, useState} from 'react'
+import classes from './App.module.scss'
 import apis from './apis/index';
+import {UsersInterface} from "./types/usersTypes";
 
 const SEED = 'TEST';
-const RESULT = 10;
+const TOTAL = 100; // 100개 의 랜덤유저가 있다고 과정
 
-function App() {
+
+interface UserProps {
+    targetUser: UsersInterface;
+}
+
+const User = ({targetUser}: UserProps): React.ReactElement => {
+    return (
+        <section className={classes.appUser}>
+            <img src={targetUser.picture.thumbnail} alt='user image' width="48" height="48"/>
+            <div className={classes.userInfo}>
+                <p>
+                    {targetUser.name.last} {targetUser.name.first}
+                </p>
+                <p>
+                    {targetUser.cell}
+                </p>
+                <p>
+                    {targetUser.email}
+                </p>
+            </div>
+            <button className={classes.passButton}>
+                PASS
+            </button>
+        </section>
+    )
+};
+
+export default function App(): React.ReactElement {
     const [count, setCount] = useState(0);
     const [page, setPage] = useState(1);
+    const [users, setUsers] = useState<UsersInterface[] | []>([]);
+
+    const findUsers = async (): Promise<void> => {
+        const result = await apis.userRepository.findUsers(page, SEED, TOTAL);
+        if (result.status === 200) {
+            const targetUsers: UsersInterface[] | [] = [...result.data.results];
+            setUsers(targetUsers);
+        } else {
+            console.error('api error => userRepository.findUsers');
+            console.error(result);
+        }
+    };
 
     useEffect(() => {
-        const findUsers = async () => {
-            const result = await apis.userRepository.findUsers(page, SEED, RESULT);
-            console.log(result);
-        }
         findUsers();
     }, []);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>Hello Vite + React!</p>
-                <p>
-                    <button type="button" onClick={() => setCount((count) => count + 1)}>
-                        count is: {count}
-                    </button>
-                </p>
-                <p>
-                    Edit <code>App.tsx</code> and save to test HMR updates.
-                </p>
-                <p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    {' | '}
-                    <a
-                        className="App-link"
-                        href="https://vitejs.dev/guide/features.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Vite Docs
-                    </a>
-                </p>
-            </header>
+        <div className={classes.app}>
+            {
+                users.map(user => {
+                    return (
+                        <>
+                            <User targetUser={user}/>
+                        </>
+                    )
+                })
+            }
         </div>
     )
 }
-
-export default App
