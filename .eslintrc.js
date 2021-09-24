a07ce5c1d5e77a85a6814dae22df564787cfc1b2
z1004man@naver.com <z1004man@naver.com>
2021-09-24 21:51:44 +0900

    [feature] 프로젝트 생성 - eslint, prettier, sass 모듈 추가 및 설정

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': ['error', { singleQuote: true, semi: true }],
+        'no-unused-vars': 'warn',
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        'no-empty-function': 'off',
+    },
+};
